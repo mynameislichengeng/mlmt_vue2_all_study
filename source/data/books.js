c7aef136561dd7a4c1120e2097b737cf88f9bfa3
李成 <licheng01@mucang.cn>
2024-03-12 09:40:41 +0800

    feat:2024-03-12实战

--- source/data/books.js
+++ source/data/books.js
@@ -1,8 +1,16 @@
 const books = [
     {
-
+    "id":1,
+    "name": '精通Vue',
+    "data": '2006-9',
+    "price": 85.00,
+    "count": 1
     },
     {
-        
+    "id":2,
+    "name": '小程序实战教程',
+    "data": '2006-2',
+    "price": 59.00,
+    "count": 1
     }
 ]
